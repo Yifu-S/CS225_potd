99b01c73761558a72de64473a60f6610d7937d04
Kolleispara <76485210+Kolleispara@users.noreply.github.com>
2021-03-01 23:12:33 +0800

    Add files via upload

---
+++ potd-q18/Node.cpp
@@ -0,0 +1,71 @@
+#include "Node.h"
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void PrintList(Node *head) {
+  if (head == NULL) {
+    cout << "Empty list" << endl;
+    return;
+  }
+
+  Node *temp = head;
+  int count = 0;
+  while(temp != NULL) {
+    cout << "Node " << count << ": " << temp ->data_ << endl;
+    count++;
+    temp = temp->next_;
+  }
+}
+
+void mergeList(Node *first, Node *second) {
+  // your code here!
+  if(first == NULL){
+    first = second;
+  }
+  if(second == NULL){
+    return;
+  }
+  Node *first_copy = first;
+  Node *second_copy = second;
+
+  while((first_copy->next_ != NULL) && (second_copy->next_ != NULL)){
+    Node *temp_first = first_copy;
+    Node *temp_second = second_copy;
+
+    first_copy = first_copy -> next_;
+    second_copy = second_copy -> next_;
+
+    temp_first -> next_ = temp_second;
+    temp_second -> next_ = first_copy;
+  }
+
+  if(first_copy->next_ == NULL){
+    first_copy->next_ = second_copy;
+  }
+  else{//second_copy->next_ == NULL
+    Node *temp = first_copy;
+    first_copy = first_copy -> next_;
+    temp -> next_ = second_copy;
+    second_copy -> next_ = first_copy;
+  }
+}
+
+Node::Node() {
+    numNodes++;
+}
+
+Node::Node(Node &other) {
+    this->data_ = other.data_;
+    this->next_ = other.next_;
+    numNodes++;
+}
+
+Node::~Node() {
+    numNodes--;
+}
+
+int Node::numNodes = 0;
+
