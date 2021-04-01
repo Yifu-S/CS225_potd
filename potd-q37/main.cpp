fec39a84b5b3ce38431c59fbbd3b47496b70f2fd
Kolleispara <76485210+Kolleispara@users.noreply.github.com>
2021-04-02 02:18:50 +0800

    Add files via upload

---
+++ potd-q37/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "functions.h"
+
+int main() {
+  // std::cout <<  << std::endl;
+  // std::cout << validUsername("1") << std::endl;
+  // std::cout << arrayIndex(-1) << std::endl;
+  // std::cout << countPrimes(3) << std::endl;
+  // std::cout << factorial(12) << std::endl;
+  // std::cout << absolutely(INT32_MIN) << std::endl;
+  std::cout << samesies(INT32_MAX, INT32_MAX) << std::endl;
+  return 0;
+}
